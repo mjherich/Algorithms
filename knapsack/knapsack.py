4b258283a53f459f7cd24973331ba661b655fc76
Matt Herich <mjherich@gmail.com>
2020-02-26 11:44:27 -0800

    Begin knapsack solution using dynamic programming principles

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,12 +1,87 @@
 #!/usr/bin/python
 
-import sys
+import sys, pdb
 from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
+class Knapsack:
+  # Invoking Knapsack without arguments should create an empty Knapsack of cost and value 0
+  def __init__(self, items=[], value=0, cost=0):
+    self.items = items
+    self.value = value
+    self.cost = cost
+
+
 def knapsack_solver(items, capacity):
-  pass
+  """
+  Uses dynamic programming to iteratively builds a grid of smaller subproblems.
+     1, 2, 3, 4, 5, 6, 7   <- COST  
+  [ [*, *, *, *, *, *, *], 👇 ITEMS 
+    [*, *, *, *, *, *, *],
+    [*, *, *, *, *, *, *],
+    [*, *, *, *, *, *, *] ]
+
+  Returns the Item indicies to select, the total value and total cost
+  ex: Items to select: 2, 8, 10
+      Total cost: 98
+      Total value: 117
+  """
+  # === Create dynamic grid ===
+  # Each grid element represents a Knapsack that maximizes the value for the given cost.
+  # n: Number of items to choose from (rows)
+  n = len(items)
+  # m: Item cost breakpoints, continuous integer breakpoints from 1 to max capacity (columns)
+  m = capacity
+  # Initilize subproblem grid with empty Knapsacks (subproblems[item_idx][knapsack_capacity])
+  subproblems = [[Knapsack()] * m] * n
+
+  # Update first row with first item solution
+  first_item = items[0]
+  first_item_value = first_item.value
+  first_item_cost = first_item.size
+  for knapsack_capacity in range(1, m):
+    if first_item_cost <= knapsack_capacity:
+      subproblems[0][knapsack_capacity] = Knapsack([first_item], first_item_value, first_item_cost)
+
+  # === Start building subproblems grid with solutions ===
+  # Loop over each subsequent subproblem and find max value given the cost (m)
+  for item_idx in range(1, n):
+    cur_item = items[item_idx]
+    cur_item_cost = cur_item.size
+    cur_item_value = cur_item.value
+    for knapsack_capacity in range(1, m):
+      knapsack_items = []
+      knapsack_value = 0
+      knapsack_cost = 0
+      # Check if the item is small enough to fit in the knapsack
+      if cur_item_cost <= knapsack_capacity:
+        knapsack_items += [cur_item]
+        knapsack_value += cur_item_value
+        knapsack_cost += cur_item_cost
+        left_over_cost = knapsack_capacity - knapsack_cost
+        # If there is left over cost...
+        if left_over_cost > 0:
+          # what item(s) can fit in the left over space that maximizes the value
+          left_over_knapsack_maxed = subproblems[item_idx-1][left_over_cost] # Returns instance of Knapsack
+          # Update knapsack value to include the left_over_knapsack value
+          knapsack_value += left_over_knapsack_maxed.value
+          # Add left_over_knapsack items to knapsack_items
+          knapsack_items.extend(left_over_knapsack_maxed.items)
+          # Update cost
+          knapsack_cost += left_over_knapsack_maxed.cost
+
+        # Save new Knapsack to subproblems grid
+        subproblems[item_idx][knapsack_capacity] = Knapsack(knapsack_items, knapsack_value, knapsack_cost)
+      else:
+        # Item doesn't fit in this subproblem, use previously found max solution
+        subproblems[item_idx][knapsack_capacity] = subproblems[item_idx-1][knapsack_capacity]
+
+  ret = {
+    "Value": sum([item.value for item in subproblems[-1][-1].items]),
+    "Chosen": [item.index for item in subproblems[-1][-1].items]
+  }
+  return ret
   
 
 if __name__ == '__main__':
@@ -21,6 +96,8 @@ if __name__ == '__main__':
       items.append(Item(int(data[0]), int(data[1]), int(data[2])))
     
     file_contents.close()
+
+    # print(items[0].index, items[0].size, items[0].value)
     print(knapsack_solver(items, capacity))
   else:
     print('Usage: knapsack.py [filename] [capacity]')
